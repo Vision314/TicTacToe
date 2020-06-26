9c04b8e3e2dd9b2ebd248011635d3b1fe15b220a
Vision314 <42813883+Vision314@users.noreply.github.com>
2020-06-25 22:24:38 -0400

    Update GameBoard.py

--- GameBoard.py
+++ GameBoard.py
@@ -5,8 +5,7 @@ class GameBoard:
         self.board = [["" for i in range(3)] for j in range(3)]
 
         self.populate(spots)
-        
-// populate method puts the X's and O's into the Board
+
     def populate(self, init):
         n = 0
         for i in range(len(self.board)):
@@ -18,7 +17,7 @@ class GameBoard:
                 elif(init[n] == 2):
                     self.board[i][j] = " O ";
                 n += 1
-// xWins determines if player X won the round
+
     def xWins(self, spots):
         if((spots[0] == 1 and spots[1] == 1 and spots[2] == 1) or
            (spots[3] == 1 and spots[4] == 1 and spots[5] == 1) or
@@ -31,8 +30,6 @@ class GameBoard:
             return True
         else:
             return False
-            
-// oWins determines if player O won the round
     def oWins(self, spots):
         if((spots[0] == 2 and spots[1] == 2 and spots[2] == 2) or
            (spots[3] == 2 and spots[4] == 2 and spots[5] == 2) or
@@ -51,7 +48,13 @@ class GameBoard:
         j = 0
         for i in range(len(self.board)):
             if(j == 1 or j == 2):
-                str += "\n-----------\n"
+                str += "\n-----------\t-----------\n"
             str += self.board[i][0] + "|" + self.board[i][1] + "|" + self.board[i][2]
+            if(i == 0):
+                str += "\t 1 | 2 | 3 "
+            elif(i == 1):
+                str += "\t 4 | 5 | 6 "
+            elif(i == 2):
+                str += "\t 7 | 8 | 9 "
             j += 1
         return str
